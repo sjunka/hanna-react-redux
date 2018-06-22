27ab1c46abb7df2495fd2a9845f74698304e1281
Sergio Junca <sergio.junca@gmail.com>
2018-06-22 01:02:21 -0500

    Phase 1 23:43

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import { Grid } from "react-bootstrap";
+
+import Subtotal from "./components/Subtotal/Subtotal";
+import  PickupSavings  from "./components/PickupSavings/PickupSavings";
+
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-      <div >
-    <h1>hola mundo</h1>
-      </div>
-    );
-  }
+    state = {
+        total: 100,
+        PickupSavings: -3.85
+    };
+    render() {
+        return (
+            <div className="container">
+                <Grid className=" purchased-card">
+                    <Subtotal price={this.state.total.toFixed(2)} />
+                    <PickupSavings price={this.state.PickupSavings}/>
+                     
+                </Grid>
+            </div>
+        );
+    }
 }
 
 export default App;
