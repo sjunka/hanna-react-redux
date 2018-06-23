d80ee4082c8d791e452c39d65f743cfd50090be8
Sergio Junca <sergio.junca@gmail.com>
2018-06-22 23:07:06 -0500

    Created components PickupSavings, TaxesFees, EstimatedTotal

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import { Grid } from "react-bootstrap";
 
 import Subtotal from "./components/Subtotal/Subtotal";
 import  PickupSavings  from "./components/PickupSavings/PickupSavings";
+import  TaxesFees  from "./components/TaxesFees/TaxesFees";
+import EstimatedTotal from "./components/EstimatedTotal/EstimatedTotal"
 
 import "./App.css";
 
 class App extends Component {
     state = {
         total: 100,
-        PickupSavings: -3.85
+        PickupSavings: -3.85,
+        taxes:0
     };
     render() {
         return (
@@ -17,7 +20,11 @@ class App extends Component {
                 <Grid className=" purchased-card">
                     <Subtotal price={this.state.total.toFixed(2)} />
                     <PickupSavings price={this.state.PickupSavings}/>
-                     
+                     <TaxesFees taxes={this.state.taxes.toFixed(2)}/>
+
+                     <EstimatedTotal price={this.state.total.toFixed(2)}/>
+                     <hr/>
+
                 </Grid>
             </div>
         );
