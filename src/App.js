eba0e99848f3507819a292d6f2ba3639b14f0749
Sergio Junca <sergio.junca@gmail.com>
2018-06-23 11:55:17 -0500

    Finished Components: EstimatedTotal ITemsDetails PromoCodes TaxesFees

--- src/App.js
+++ src/App.js
@@ -5,15 +5,36 @@ import Subtotal from "./components/Subtotal/Subtotal";
 import  PickupSavings  from "./components/PickupSavings/PickupSavings";
 import  TaxesFees  from "./components/TaxesFees/TaxesFees";
 import EstimatedTotal from "./components/EstimatedTotal/EstimatedTotal"
+import  ItemDetails  from "./components/ItemsDetails/ItemDetails";
+import PromoCode from "./components/PromoCode/PromoCode"
+
 
 import "./App.css";
 
 class App extends Component {
     state = {
-        total: 100,
+        total: 200,
         PickupSavings: -3.85,
-        taxes:0
+        taxes:0,
+        estimatedTotal:0,
+        disablePromoButton: false, 
+
     };
+
+    componentDidMount = () => {
+      this.setState({
+          taxes: (this.state.total + this.state.PickupSavings) * 0.0875
+      }, () => {
+          this.setState({
+            estimatedTotal: this.state.total + this.state.PickupSavings + this.state.taxes
+          })
+      })
+    }
+    
+
+
+
+
     render() {
         return (
             <div className="container">
@@ -22,8 +43,15 @@ class App extends Component {
                     <PickupSavings price={this.state.PickupSavings}/>
                      <TaxesFees taxes={this.state.taxes.toFixed(2)}/>
 
-                     <EstimatedTotal price={this.state.total.toFixed(2)}/>
                      <hr/>
+                     <EstimatedTotal price={this.state.estimatedTotal.toFixed(2)}/>
+                    <ItemDetails price={this.state.estimatedTotal.toFixed(2)}/>
+
+            <hr/>
+                <PromoCode
+                giveDiscount={() => this.giveDiscountHandler()}
+                isDisabled = {this.state.disabledPromoButton}
+                />
 
                 </Grid>
             </div>
