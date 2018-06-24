42af5cac484f359f731adf103e723908ad7a5021
Sergio Junca <sergio.junca@gmail.com>
2018-06-24 00:19:18 -0500

    Added Redux integration

--- src/App.js
+++ src/App.js
@@ -2,61 +2,89 @@ import React, { Component } from "react";
 import { Grid } from "react-bootstrap";
 
 import Subtotal from "./components/Subtotal/Subtotal";
-import  PickupSavings  from "./components/PickupSavings/PickupSavings";
-import  TaxesFees  from "./components/TaxesFees/TaxesFees";
-import EstimatedTotal from "./components/EstimatedTotal/EstimatedTotal"
-import  ItemDetails  from "./components/ItemsDetails/ItemDetails";
-import PromoCode from "./components/PromoCode/PromoCode"
+import PickupSavings from "./components/PickupSavings/PickupSavings";
+import TaxesFees from "./components/TaxesFees/TaxesFees";
+import EstimatedTotal from "./components/EstimatedTotal/EstimatedTotal";
+import ItemDetails from "./components/ItemsDetails/ItemDetails";
+import PromoCode from "./components/PromoCode/PromoCode";
 
+import { connect } from "react-redux";
+
+import { handleChange } from "./actions/promoCodeActions";
 
 import "./App.css";
 
 class App extends Component {
     state = {
-        total: 200,
+        total: 100,
         PickupSavings: -3.85,
-        taxes:0,
-        estimatedTotal:0,
-        disablePromoButton: false, 
-
+        taxes: 0,
+        estimatedTotal: 0,
+        disablePromoButton: false
     };
 
     componentDidMount = () => {
-      this.setState({
-          taxes: (this.state.total + this.state.PickupSavings) * 0.0875
-      }, () => {
-          this.setState({
-            estimatedTotal: this.state.total + this.state.PickupSavings + this.state.taxes
-          })
-      })
-    }
-    
+        this.setState(
+            {
+                taxes: (this.state.total + this.state.PickupSavings) * 0.0875
+            },
+            () => {
+                this.setState({
+                    estimatedTotal:
+                        this.state.total +
+                        this.state.PickupSavings +
+                        this.state.taxes
+                });
+            }
+        );
+    };
 
+    giveDiscountHandler = () => {
+        if (this.props.promoCode === "DISCOUNT420") {
+            this.setState(
+                {
+                    estimatedTotal: this.state.estimatedTotal * 0.9
+                },
 
-
+                () => {
+                    this.setState({
+                        disablePromoButton: true
+                    });
+                }
+            );
+        }
+    };
 
     render() {
         return (
             <div className="container">
                 <Grid className=" purchased-card">
                     <Subtotal price={this.state.total.toFixed(2)} />
-                    <PickupSavings price={this.state.PickupSavings}/>
-                     <TaxesFees taxes={this.state.taxes.toFixed(2)}/>
+                    <PickupSavings price={this.state.PickupSavings} />
+                    <TaxesFees taxes={this.state.taxes.toFixed(2)} />
 
-                     <hr/>
-                     <EstimatedTotal price={this.state.estimatedTotal.toFixed(2)}/>
-                    <ItemDetails price={this.state.estimatedTotal.toFixed(2)}/>
+                    <hr />
+                    <EstimatedTotal
+                        price={this.state.estimatedTotal.toFixed(2)}
+                    />
+                    <ItemDetails price={this.state.estimatedTotal.toFixed(2)} />
 
-            <hr/>
-                <PromoCode
-                giveDiscount={() => this.giveDiscountHandler()}
-                isDisabled = {this.state.disabledPromoButton}
-                />
-
+                    <hr />
+                    <PromoCode
+                        giveDiscount={() => this.giveDiscountHandler()}
+                        isDisabled={this.state.disablePromoButton}
+                    />
                 </Grid>
             </div>
         );
     }
 }
 
-export default App;
+const mapStateToProps = state => ({
+    promoCode: state.promoCode.value
+});
+
+export default connect(
+    mapStateToProps,
+    { handleChange }
+)(App);
